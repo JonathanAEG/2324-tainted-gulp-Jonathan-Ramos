c501b0f71a8932a9878d5e02612d89d31c71938e
Jonathan <jonathan.ramos@ikasle.aeg.eus>
2023-09-25 10:13:31 +0200

    [FIX] Arreglado problema de no asignar los valores correctamente en el constructor

--- Character.mjs
+++ Character.mjs
@@ -3,7 +3,11 @@ export default class Character{
 
     constructor(fullName, health, magick, stamina, potions){
 
-
+        this.fullName = fullName;
+        this.healt = health;
+        this.magick = magick;
+        this.stamina = stamina;
+        this.potions = potions;
     }
 
     static from({name, health, magick, stamina}, potions){
